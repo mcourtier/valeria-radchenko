c3c69c8539086eb155bb280c3a816229b6c5c490
Michael Courtier <michael.courtier@gmail.com>
2023-07-14 18:41:30 +0200

    feat: add tailwind + robot no index

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <header></header>
+    <main>
+        <section class="text-2xl text-center p-10">
+            Valeria Radchenko
+        </section>
+    </main>
+    <footer></footer>
+</template>
