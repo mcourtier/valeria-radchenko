eebbd707bb3fcd44724dc8f9097fab6f60977b5c
Michael Courtier <michael.courtier@gmail.com>
2023-07-18 00:15:35 +0200

    feat: add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,25 @@ module.exports = {
       "./nuxt.config.{js,ts}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          brand: {
+            50: "#F7F3ED",
+            100: "#F0E8DD",
+            200: "#E0CFB8",
+            300: "#D0B795",
+            400: "#C1A071",
+            500: "#B1884E",
+            600: "#8E6D3E",
+            700: "#6A512F",
+            800: "#47361F",
+            900: "#231B10",
+            950: "#120E08"
+          },
+          // ...
+        },
+    
+      },
     },
     plugins: [],
   }
