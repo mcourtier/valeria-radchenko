0a785a81f6ffc2ed4a441e43029fa0983661f9e5
Michael Courtier <michael.courtier@gmail.com>
2023-07-22 15:57:54 +0200

    feat: seo stuffs

--- components/slice/Grid.vue
+++ components/slice/Grid.vue
@@ -2,7 +2,11 @@
   <VSlice>
     <div class="grid grid-cols-2 gap-2">
       <div v-for="item in items" class="text-xs">
-        <img :src="item.image" class="object-fit w-full h-32" />
+        <img
+          :src="item.image"
+          class="object-fit w-full h-32"
+          :alt="item.label"
+        />
         <div class="font-bold mb-1">{{ item.label }}</div>
         <div class="">{{ item.content }}</div>
       </div>
@@ -19,22 +23,22 @@ type GridItem = {
 const items = [
   {
     label: "Est pariatur",
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/200/128",
     content: "Excepteur tempor dolor pariatur labore sit magna non.",
   },
   {
     label: "Deserunt dolor occaecat",
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/200/128",
     content: "Aliqua id ex eu cillum ex excepteur irure.",
   },
   {
     label: "Do qui qui magna minim et",
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/200/128",
     content: "Consectetur labore ad Lorem elit sint.",
   },
   {
     label: "Dolore exercitation sunt ullamco",
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/200/128",
     content:
       "Aute nostrud Lorem consequat sit esse ipsum incididunt in qui aute.",
   },
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,8 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       meta: [
         { hid: 'robots', name: 'robots', content: 'noindex' },
-        { hid: 'googlebot', name: 'googlebot', content: 'noindex' }
+        { hid: 'googlebot', name: 'googlebot', content: 'noindex' },
+        { hid: 'description', name: 'description', content: 'Veniam magna proident ipsum aute ipsum sit. Mollit ut officia aute fugiat Lorem enim quis aliquip velit duis irure anim aliquip.' }
       ]
     },
     baseURL: "/valeria/"
