5dbffaf79ee143f801d0792697c6de6aa0b56169
Michael Courtier <michael.courtier@gmail.com>
2023-07-23 21:35:52 +0200

    fix: aspect ratio

--- components/slice/Grid.vue
+++ components/slice/Grid.vue
@@ -23,22 +23,22 @@ type GridItem = {
 const items = [
   {
     label: "Est pariatur",
-    image: "https://picsum.photos/200/128",
+    image: "https://picsum.photos/186/128",
     content: "Excepteur tempor dolor pariatur labore sit magna non.",
   },
   {
     label: "Deserunt dolor occaecat",
-    image: "https://picsum.photos/200/128",
+    image: "https://picsum.photos/186/128",
     content: "Aliqua id ex eu cillum ex excepteur irure.",
   },
   {
     label: "Do qui qui magna minim et",
-    image: "https://picsum.photos/200/128",
+    image: "https://picsum.photos/186/128",
     content: "Consectetur labore ad Lorem elit sint.",
   },
   {
     label: "Dolore exercitation sunt ullamco",
-    image: "https://picsum.photos/200/128",
+    image: "https://picsum.photos/186/128",
     content:
       "Aute nostrud Lorem consequat sit esse ipsum incididunt in qui aute.",
   },
