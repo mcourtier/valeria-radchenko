b53006221207c255bbda82c786da60d5ad21220b
Michael Courtier <michael.courtier@gmail.com>
2023-07-23 20:32:34 +0200

    feat: optimize image

--- components/slice/Hero.vue
+++ components/slice/Hero.vue
@@ -3,7 +3,7 @@
     <div
       class="w-64 h-64 mb-8 mx-auto inline-block rounded-full overflow-hidden shadow bg-cover"
     >
-      <img src="/vr.jpg" class="w-full" alt="Valeria Radchenko" />
+      <img src="/vr-256.jpg" class="w-full" alt="Valeria Radchenko" />
     </div>
     <h1 class="font-title text-6xl font-bold mb-4">Valeria <br />Radchenko</h1>
     <h2 class="text-xl font-bold mb-8">English Teacher</h2>
