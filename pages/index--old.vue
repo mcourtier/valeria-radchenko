52737b2afe22d314908c7cb3d4831f376da411cc
Michael Courtier <michael.courtier@gmail.com>
2023-08-08 00:16:01 +0200

    feat: swap homepages

---
+++ pages/index--old.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- <VSvg svg="check" class="w-12" /> -->
+  <SliceHero />
+  <SliceList dark />
+  <SliceGrid />
+  <SliceAccordion dark />
+  <SliceCarousel />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,29 @@
+<script setup lang="ts">
+import { components } from "~/slices";
+
+const prismic = usePrismic();
+const { data: page } = useAsyncData("[homepage]", () =>
+  prismic.client.getSingle("homepage")
+);
+
+useHead({
+  title: page.value?.data.meta_title,
+  meta: [
+    {
+      name: "description",
+      content: page.value?.data.meta_description,
+    },
+  ],
+});
+
+// TODO : remove this CSR force reload
+refreshNuxtData();
+</script>
+
 <template>
-  <!-- <VSvg svg="check" class="w-12" /> -->
-  <SliceHero />
-  <SliceList dark />
-  <SliceGrid />
-  <SliceAccordion dark />
-  <SliceCarousel />
+  <SliceZone
+    wrapper="main"
+    :slices="page?.data.slices ?? []"
+    :components="components"
+  />
 </template>
